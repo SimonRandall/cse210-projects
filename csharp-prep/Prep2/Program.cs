db8dc02723dfb779164d247985b7e596e4e7974d
Simon <ran18018@byui.edu>
2023-04-23 02:13:59 -0500

    Prep 2 Changes

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        Console.Write("What is the grade percentage? ");
+        string gradeInput = Console.ReadLine();
+        int grade = int.Parse(gradeInput);
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade <90 && grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade < 70 && grade is >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"The letter grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congrats you passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed, keep trying");
+        }
+
+
     }
 }
